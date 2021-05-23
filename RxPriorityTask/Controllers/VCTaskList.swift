d425c8f28c6c7e1caad6e62474a89d0de44b4b60
Minho Yi <tisohjung@gmail.com>
2021-05-23 19:07:29 +0900

    ✨ setup BehaviorRelay to append tasks

--- RxPriorityTask/Controllers/VCTaskList.swift
+++ RxPriorityTask/Controllers/VCTaskList.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class VCTaskList: UIViewController {
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
 
+    private var tasks = BehaviorRelay<[Task]>(value: [])
+
+    let disposeBag = DisposeBag()
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController,
+              let addTaskVC = navC.viewControllers.first as? VCAddTask else {
+            fatalError("Controller not found")
+        }
+        addTaskVC.taskSubjectObservable
+            .subscribe(onNext: { task in
+                var existingTasks = self.tasks.value
+                existingTasks.append(task)
+                self.tasks.accept(existingTasks)
+            }).disposed(by: disposeBag)
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -19,7 +37,7 @@ extension VCTaskList : UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.tasks.value.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VCTaskList", for: indexPath)
